5bca851356f72e9c8d3612fbca57a3949dff9c58
Siyuan Li <siyuanl4@andrew.cmu.edu>
2024-04-18 23:29:26 -0400

    fix synchronizer

--- src/chip.sv
+++ src/chip.sv
@@ -18,11 +18,19 @@ module my_chip (
     logic [3:0] index;
 
     // input synchronization and assignment
-    Synchronizer sync1(.async(io_in[7:0]), .clock(clock), .sync(data_in[7:0]));
-    Synchronizer sync2(.async(io_in[8]), .clock(clock), .sync(enter));
-    Synchronizer sync3(.async(io_in[9]), .clock(clock), .sync(sw));
-    Synchronizer sync4(.async(io_in[11:10]), .clock(clock), .sync(operation[1:0]));
-    Synchronizer sync5(.async(reset), .clock(clock), .sync(rst));
+    Synchronizer sync1(.async(io_in[0]), .clock(clock), .sync(data_in[0]));
+    Synchronizer sync2(.async(io_in[1]), .clock(clock), .sync(data_in[1]));
+    Synchronizer sync3(.async(io_in[2]), .clock(clock), .sync(data_in[2]));
+    Synchronizer sync4(.async(io_in[3]), .clock(clock), .sync(data_in[3]));
+    Synchronizer sync5(.async(io_in[4]), .clock(clock), .sync(data_in[4]));
+    Synchronizer sync6(.async(io_in[5]), .clock(clock), .sync(data_in[5]));
+    Synchronizer sync7(.async(io_in[6]), .clock(clock), .sync(data_in[6]));
+    Synchronizer sync8(.async(io_in[7]), .clock(clock), .sync(data_in[7]));
+    Synchronizer sync9(.async(io_in[8]), .clock(clock), .sync(enter));
+    Synchronizer sync10(.async(io_in[9]), .clock(clock), .sync(sw));
+    Synchronizer sync11(.async(io_in[10]), .clock(clock), .sync(operation[0]));
+    Synchronizer sync12(.async(io_in[11]), .clock(clock), .sync(operation[1]));
+    Synchronizer sync13(.async(reset), .clock(clock), .sync(rst));
 
     // output assignment
     assign io_out[4:0] = data_out[4:0];
